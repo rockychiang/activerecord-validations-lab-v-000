4f1599d06d4bea6009378b7c5346f54a8c237c06
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 07:10:49 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+  private
+
+  def clickbait?
+    if title.present?
+      cb = ["Won't Believe", "Secret", "Top", "Guess"].map do |phrase|
+        title.include?(phrase) ? true : false
+      end
+      if !cb.include?(true)
+        errors.add(:title, "Not clickbait-y")
+      end
+    end
+  end
 end
